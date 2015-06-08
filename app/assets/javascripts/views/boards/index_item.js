5cd1059ef3789188223c3ca684509345638b3c05
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-06-08 10:00:55 -0700

    Add edit and delete buttons

--- app/assets/javascripts/views/boards/index_item.js
+++ app/assets/javascripts/views/boards/index_item.js
@@ -3,10 +3,19 @@ TrelloClone.Views.BoardsIndexItem = Backbone.View.extend({
 
   className: "boards-index-item col-md-3",
 
+  events: {
+    "click .remove-board-btn": "removeBoard"
+  },
+
   attributes: function () {
     return {
       "data-id": this.model.id
     }
+  },
+
+  removeBoard: function (event) {
+    debugger;
+    console.log("Remove Board " + event.currentTarget);
   },
 
   render: function() {
